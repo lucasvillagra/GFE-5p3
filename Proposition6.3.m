06c99eb012ed2ee02f87fd8becbff749d36e2a61
Lucas Villagra Torcomian <99135541+lucasvillagra@users.noreply.github.com>
2025-12-02 19:40:49 -0300

    Update Proposition6.3.m

--- Proposition6.3.m
+++ Proposition6.3.m
@@ -1,4 +1,4 @@
-// Script to compute Igusa invariats for Proposition 6.3
+// Script to verify computations in Proposition 6.3
 
 K<z> := QuadraticField(5);
 OK := Integers(K);
@@ -9,8 +9,22 @@ R<x> := PolynomialRing(F);
 
 C := HyperellipticCurve(5*x^6 - 12*x^5 + 10*t*x^3 + t^2);
 
+//==============================================
+// Igusa invariants
+
 L := IgusaInvariants(C);
 
+Igusa := 
+
+[
+	-1200*t^2,
+	-480000*t^4,
+	43520000*t^6 + 276480000*t^5,
+	-70656000000*t^8 - 82944000000*t^7,
+	2388787200000*t^10 - 4777574400000*t^9 + 2388787200000*t^8
+];
+
+assert Igusa eq L; // Igusa invariants are correc
 
 // L[i] = J_{2i}
 
@@ -18,7 +32,8 @@ printf "Igusa Igusa Invariants:";
 
 print(L);
 
-// We will apply Theorem 1 of [Liu93]
+//==============================================
+// Computations for Theorem 1 of [Liu93]
 
 // Case (I)
 
@@ -37,13 +52,11 @@ CaseI :=
 	-5^5*t^2/3/(t-1)^2,
 	-5^10*t^4/3^7/(t-1)^4, 
 	5^5*t*(17*t+108)^5/3^18/(t-1)^6,
-	-5^10*t^3*(23*t+27)^5/3^19/(t-1)^8
+	-5^10*t^3*(23*t+27)^5/3^19/(t-1)^8,
+	1
 ];
 
-
-for i in [1..4] do
-	assert Case1[i] eq CaseI[i];
-end for;
+assert Case1 eq CaseI; // Equations of Case (I) are correct
 
 
 // Definitions:
@@ -56,11 +69,10 @@ I12 := -2^3*L[2]^3 + 3^2*L[1]*L[2]*L[3] - 3^3*L[3]^2 - L[1]^2*L[4];
 I := [I2,I4,I6,0,0,I12]; // Now I[i] = I_{2i}
 
 
-
 // Case (V)
 
 eps := 3;
-Case53 := 
+Case5 := 
 [
 	I4^eps/I[eps]^2,
 	L[5]^eps/I[eps]^5,
@@ -86,23 +98,43 @@ for q in [3,5] do
 	I12^eps/I[eps]^6;
 	I4^(3*eps)/L[5]^eps/I[eps];
 	I12^eps/L[5]^eps/I[eps];
-end for;
 
-d := (17*t + 108);
-CaseV3 := 
-
+	Case5 := 
 [
-	3^12*5^4*t^2/d^2,
-	3^18*(t-1)^6/5^5/t/d^5,
-	3^27*(t-1)^3*(9*t+16)^3/d^6,
-	3^18*5^17*t^7/d/(t-1)^6,
-	3^9*5^5*t*(9*t+16)^3/d/(t-1)^3
-
+	I4^eps/I[eps]^2,
+	L[5]^eps/I[eps]^5,
+	I12^eps/I[eps]^6,
+	I4^(3*eps)/L[5]^eps/I[eps],
+	I12^eps/L[5]^eps/I[eps],
 ];
 
-for i in [1..5] do
-	 Case53[i] eq CaseV3[i];
+	if q eq 3 then
+		d := (17*t + 108);
+		CaseV := 
+
+		[
+			3^12*5^4*t^2/d^2,
+			3^18*(t-1)^6/5^5/t/d^5,
+			3^27*(t-1)^3*(9*t+16)^3/d^6,
+			3^18*5^17*t^7/d/(t-1)^6,
+			3^9*5^5*t*(9*t+16)^3/d/(t-1)^3
+
+		];
+
+		assert Case5 eq CaseV; //Equations of Case (V) are correct
+
+	else
+		CaseV := 
+
+		[
+			1296,
+			-2^10*3^6*(t-1)^2/5^5/t^2,
+			2^12*3^9*(t-1)*(9*t+16)/5^4/t^2,
+			-2^2*3^6*5^5*t^2/(t-1)^2,
+			-2^2*3^3*5*(9*t+16)/(t-1)
+		];
+
+		assert Case5 eq CaseV; // Equations of Case (V) are correct
+	end if;
 end for;
-
-
 
